113bc6f3ffa1f4bd18fd4e60dfb70456b4064318
Steven Wallace <steven@thewallaces.ca>
2017-03-05 01:39:48 -0800

    bootstrap

--- View.elm
+++ View.elm
@@ -5,59 +5,48 @@ import Html.Attributes exposing (class, src, style)
 import Html.Events exposing (onClick)
 import Msgs exposing (Msg)
 import Models exposing (Model, Clicker, calcCPS, totalCPS)
+import Bootstrap.Grid exposing (..)
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ img
-            [ src "images/cookie.png"
-            , style [ ( "cursor", "pointer" ) ]
-            , onClick (Msgs.OnCookieClick)
-            ]
+        [ node "link"
+            [ Html.Attributes.rel "stylesheet", Html.Attributes.href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" ]
             []
-        , h2 []
-            [ text (toString model.cookies ++ " cookies") ]
-        , h3 []
-            [ text (toString (totalCPS model) ++ " cookies per second") ]
-        , div []
-            [ clickersList model.clickers ]
-        ]
-
-
-clickersList : List Clicker -> Html Msg
-clickersList clickers =
-    div [ class "p2" ]
-        [ table []
-            [ thead []
-                [ tr []
-                    [ th [] [ text "Id" ]
-                    , th [] [ text "CPS" ]
-                    , th [] [ text "Amount" ]
-                    , th [] [ text "TCPS" ]
-                    , th [] [ text "Cost" ]
-                    , th [] [ text "Buy" ]
+        , containerFluid
+            [ row
+                [ column [ Medium Four ]
+                    [ img
+                        [ src "images/cookie.png"
+                        , style [ ( "cursor", "pointer" ) ]
+                        , onClick (Msgs.OnCookieClick)
+                        ]
+                        []
+                    , h2 []
+                        [ text (toString model.cookies ++ " cookies") ]
+                    , h3 []
+                        [ text (toString (totalCPS model) ++ " cookies per second") ]
                     ]
+                , column [ Medium Four ]
+                    (List.map clickerBox model.clickers)
                 ]
-            , tbody [] (List.map clickerRow clickers)
             ]
         ]
 
 
-clickerRow : Clicker -> Html Msg
-clickerRow clicker =
-    tr []
-        [ td [] [ text clicker.id ]
-        , td [] [ text (toString clicker.cps) ]
-        , td [] [ text (toString clicker.amount) ]
-        , td [] [ text (toString (calcCPS clicker)) ]
-        , td [] [ text (toString clicker.cost) ]
-        , td []
-            [ button
-                [ onClick (Msgs.Buy clicker 1) ]
-                [ text "buy" ]
-            , button
-                [ onClick (Msgs.Sell clicker 1) ]
-                [ text "sell" ]
+clickerBox : Clicker -> Html Msg
+clickerBox clicker =
+    div []
+        [ strong [] [ text clicker.id ]
+        , div [] [ text ("Price: " ++ (toString clicker.cost)) ]
+        , div [] [ text ("Owned: " ++ (toString clicker.amount)) ]
+        , div []
+            [ button [ onClick (Msgs.Buy clicker 1) ] [ text "Buy" ]
+            , button [ onClick (Msgs.Sell clicker 1) ] [ text "Sell" ]
             ]
+        , span []
+            [ text ("CPS: " ++ (toString clicker.cps)) ]
+        , span []
+            [ text ("TCPS: " ++ (toString (calcCPS clicker))) ]
         ]
